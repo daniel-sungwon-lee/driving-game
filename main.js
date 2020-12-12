12f01228fd21f871aef63985b391caf648e79413
Daniel Sungwon Lee <danlee333@gmail.com>
2020-12-11 16:05:01 -0800

    completed issue-3-start-car

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 var $car = document.querySelector("#car")
+var i =1
 
 document.addEventListener("keydown",function(event){
   if (event.code==="ArrowRight"){
-    $car.className="car east transition"
+    $car.className= data.east
+    setInterval(function move(){
+      i++
+      var stringNum=i.toString()
+      $car.style.left=stringNum+"rem"
+    },16)
   } else if (event.code==="ArrowDown"){
-    $car.className="car south transition"
+    $car.className= data.south
   } else if (event.code==="ArrowLeft"){
-    $car.className="car west transition"
+    $car.className=data.west
   } else if (event.code==="ArrowUp"){
-    $car.className="car north transition"
+    $car.className=data.north
   }
 })
+
+var data = {
+  north: "car north transition",
+  south: "car south transition",
+  east: "car east transition",
+  west: "car west transition"
+}
