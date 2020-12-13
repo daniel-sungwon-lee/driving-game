780715d0d33690b32059a27c6bb4ba984c8b0c07
Daniel Sungwon Lee <danlee333@gmail.com>
2020-12-12 17:37:22 -0800

    made edits to code

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var $car = document.querySelector("#car")
+var $carImg = document.querySelector("#car-img")
 
 function Car(direction) {
   this.direction = direction
@@ -6,8 +7,13 @@ function Car(direction) {
   this.started = false
 }
 
-var car =null
+var car = new Car()
 var start = null
+
+var x = getComputedStyle($car).left
+var y = getComputedStyle($car).top
+car.location.push(parseInt(x))
+car.location.push(parseInt(y))
 
 function moveEast() {
   var i = car.location[0]
@@ -48,71 +54,51 @@ function moveNorth() {
 document.addEventListener("keydown",function(event){
 
   if (event.code==="ArrowRight"){
-    car=new Car("east")
-    $car.className= "car east transition"
-
-    var x = getComputedStyle($car).left
-    var y = getComputedStyle($car).top
-    car.location.push(parseInt(x))
-    car.location.push(parseInt(y))
+    car.direction="east"
+    $car.className= "car east"
 
     clearInterval(start)
-    car.started = false
+    car.started=false
 
-    if (car === null ||car.started===false){
+    if (car.started===false){
       start=setInterval(moveEast,16)
       car.started=true
     }
 
   } else if (event.code==="ArrowDown"){
-    car =new Car("south")
-    $car.className= "car south transition"
-
-    var x = getComputedStyle($car).left
-    var y = getComputedStyle($car).top
-    car.location.push(parseInt(x))
-    car.location.push(parseInt(y))
+    car.direction="south"
+    $car.className= "car south"
 
     clearInterval(start)
     car.started = false
 
-    if (car === null || car.started === false){
+    if (car.started === false){
       start=setInterval(moveSouth,16)
       car.started = true
     }
 
   } else if (event.code==="ArrowLeft"){
-    car =new Car("west")
-    $car.className= "car west transition"
-
-    var x = getComputedStyle($car).left
-    var y = getComputedStyle($car).top
-    car.location.push(parseInt(x))
-    car.location.push(parseInt(y))
+    car.direction="west"
+    $car.className= "car west"
 
     clearInterval(start)
     car.started = false
 
-    if (car === null || car.started === false){
+    if (car.started === false){
       start = setInterval(moveWest,16)
       car.started = true
     }
 
   } else if (event.code==="ArrowUp"){
-    car =new Car("north")
-    $car.className= "car north transition"
-
-    var x = getComputedStyle($car).left
-    var y = getComputedStyle($car).top
-    car.location.push(parseInt(x))
-    car.location.push(parseInt(y))
+    car.direction="north"
+    $car.className= "car north"
 
     clearInterval(start)
-    car.started=false
+    car.started = false
 
-    if (car === null || car.started === false){
+    if (car.started === false){
       start=setInterval(moveNorth,16)
-      car.started = true
+      car.started=true
     }
 
   }else if (event.code==="Space"){
